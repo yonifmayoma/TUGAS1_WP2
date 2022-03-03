395196b60c230353febb2f03f7bd0c9d396c55ab
Yonif Mayoma <74245770+yonifmayoma@users.noreply.github.com>
2022-03-03 19:29:36 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,12 @@
+<?php
+
+ $nama = "Yonif Mayoma"; 
+ $nim = "20090111";  
+ $kelas = "4B";
+
+ echo "Halo! Perkenalkan Saya,\n";
+ echo "Nama     : $nama\n";
+ echo "Nim      : $nim\n";
+ echo "Kelas    : $kelas\n";
+
+?>
